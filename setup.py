e292945985c6ab065f99e9152c8bc61c068333ec
JustNevi <slavkad25@gmail.com>
2025-09-06 15:46:31 +0200

    build: change package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="osmxml",
-    version="1.0.0",
+    version="1.0.1",
 
     author="osmiumnet",
     description="Python object-oriented xml package",
